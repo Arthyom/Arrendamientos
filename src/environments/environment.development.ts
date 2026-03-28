2d5c85ad2dc38d1790a5f84b7ac9b480622bdc49
= <=>
2026-03-27 23:24:02 -0600

    Updating url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // backEndBaseUrl: 'https://www.apiarrendamiento.alfredowflsyopollo.us/api',
-  backEndBaseUrl:"http://localhost:5165/api"
+  backEndBaseUrl: 'https://www.apiarrendamiento.alfredowflsyopollo.us/api',
+  // backEndBaseUrl:"http://localhost:5165/api"
 };
